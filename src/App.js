e12d58ee229477034d1b1e04e8d28c661dd302bc
rado0999 <radoxml@gmail.com>
2023-09-11 14:52:47 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import NavBar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-  <h1>Hello world</h1>
+      <div>
+        <NavBar />
+      </div>
+      <h1>Hello world</h1>
     </div>
   );
 }
